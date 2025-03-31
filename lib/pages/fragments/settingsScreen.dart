53c887ead6fbcfb2e1ef3d5b26cbeab6ca148170
Sanzida-Islam-Smriti <islam15-4835@diu.edu.bd>
2025-03-31 19:06:06 +0600

    Update

--- lib/pages/fragments/settingsScreen.dart
+++ lib/pages/fragments/settingsScreen.dart
@@ -7,9 +7,9 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // child: Text(
-        //     "Settings"
-        // ),
+        child: Text(
+            "SettingScreen"
+        ),
       ),
     );
   }
